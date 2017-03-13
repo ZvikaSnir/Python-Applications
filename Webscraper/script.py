54a364f3c89a422f502dead7fed5d87a903ede18
DannyLuong408 <dannyluong408@gmail.com>
2017-03-12 18:32:37 -0700

    update

---
+++ Webscraper/script.py
@@ -0,0 +1,58 @@
+import requests, pandas
+from bs4 import BeautifulSoup
+
+
+r=requests.get("http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/")
+c=r.content
+
+soup=BeautifulSoup(c,"html.parser")
+page_nr=soup.find_all("a",{"class":"Page"})[-1].text
+l=[]
+
+base_url="http://pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/t=0&s="
+for page in range(0,int(page_nr)*10,10):
+    print(base_url+str(page)+".html")
+    r=requests.get(base_url+str(page)+".html")
+    c=r.content
+    soup=BeautifulSoup(c,"html.parser")
+    all=soup.find_all("div",{"class":"propertyRow"})
+
+    for item in all:
+        d={}
+        d["Address"]=item.find_all("span",{"class":"propAddressCollapse"})[0].text
+        try:
+            d["Locality"]=item.find_all("span",{"class":"propAddressCollapse"})[1].text
+        except:
+            d["Locality"]=None
+        d["Price"]=item.find("h4",{"class":"propPrice"}).text.replace("\n","").replace(" ","")
+
+        try:
+            d["Beds"]=item.find("span",{"class":"infoBed"}).find("b").text
+        except:
+            d["Beds"]=None
+
+        try:
+            d["Area"]=item.find("span",{"class":"infoSqFt"}).find("b").text
+        except:
+            d["Area"]=None
+
+        try:
+            d["Full Bath"]=item.find("span",{"class":"infoValueFullBath"}).find("b").text
+        except:
+            d["Full Bath"]=None
+
+        try:
+            d["Full Bath"]=item.find("span",{"class":"infoValueHalfBath"}).find("b").text
+        except:
+            d["Full Bath"]=None
+
+        for column_group in item.find_all("div",{"class":"columnGroup"}):
+            #print(column_group)
+            for feature_group, feature_name in zip(column_group.find_all("span",{"class":"featureGroup"}),column_group.find_all("span",{"class":"featureName"})):
+                #print(feature_group.text, feature_name.text)
+                if "Lot Size" in feature_group.text:
+                    d["Lot Size"]=feature_name.text
+        l.append(d)
+
+df=pandas.DataFrame(l)
+df.to_csv("Output.csv")
